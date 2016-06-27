5255e017078a234155fe8440c514300731051fac
Patrick Fong <pafong@groupon.com>
2016-06-26 23:36:03 -0700

    introduce concept of songs

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,11 @@
+class SongsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+  
+end
---
+++ app/models/song.rb
@@ -0,0 +1,12 @@
+class Song < ActiveRecord::Base
+  
+  VALID_KEY_NAMES = %w(Ab A Bb B C C# D Eb E F F# G G#)
+  validates_inclusion_of :key, in: VALID_KEY_NAMES, allow_nil: true
+  before_save :normalize
+
+  private
+  def normalize
+    self.name = self.name.titleize
+    self.artist = self.name.titleize
+  end
+end
