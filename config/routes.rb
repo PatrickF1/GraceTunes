75e7a3a6a3b6a2e121d0240364e54431471bb869
Patrick Fong <pafong@groupon.com>
2016-06-27 23:00:52 -0700

    add routes for songs and tags resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'songs#index'
+
+  resources :songs
+  resources :tags
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
