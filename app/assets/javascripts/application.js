a2005712e4c2273d7732c8da3ef667f16b2bbf43
leethomas <leet944@gmail.com>
2016-07-17 22:03:30 -0700

    add searchable songs table & necessary query

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
+//= require dataTables/jquery.dataTables
 //= require turbolinks
 //= require_tree .
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,34 @@
 class SongsController < ApplicationController
   def index
+    respond_to do |format|
+      format.json do
+        songs = Song.all
+        total_songs = songs.count
+
+        if params[:sSearch].present?
+          search = {
+            query: "%#{params[:sSearch]}%",
+            rQuery: Regexp.escape(params[:sSearch])
+          }
+
+          songs = songs.where('name LIKE :query OR artist LIKE :query OR
+            song_sheet REGEXP :rQuery', search)
+        end
+
+        song_data = {
+          draw: params[:draw].to_i,
+          recordsTotal: total_songs,
+          recordsFiltered: total_songs - songs.count,
+          data: songs
+        }
+
+        render json: song_data and return
+      end
+
+      format.html do
+        return
+      end
+    end
   end
 
   def new
