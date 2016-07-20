faa72b42e2b7a1580665dd9aa54aabebd87daf2f
Patrick Fong <pafong@groupon.com>
2016-07-20 13:46:42 -0700

    sanitize SQL fragments going into SongsFinder

--- app/finders/songs_finder.rb
+++ app/finders/songs_finder.rb
@@ -1,17 +1,18 @@
 class SongsFinder
   
-  def self.search(query)
-    return [] if query.blank?
-    
+  def self.search(keywords)
+    return [] if keywords.blank?
+    weighted_query = ActiveRecord::Base::sanitize(">(#{keywords})")
+    unweighted_query = ActiveRecord::Base::sanitize(keywords)
     Song.find_by_sql <<-SQL.squish
       SELECT
         *,
-        MATCH (name) AGAINST ('>(#{query})' IN BOOLEAN MODE) +
-        MATCH (song_sheet) AGAINST ('(#{query})' IN BOOLEAN MODE) AS relevance
+        MATCH (name) AGAINST (#{weighted_query} IN BOOLEAN MODE) +
+        MATCH (song_sheet) AGAINST (#{unweighted_query} IN BOOLEAN MODE) AS relevance
       FROM 
         songs 
       WHERE 
-        MATCH(name, song_sheet) AGAINST ('#{query}' IN BOOLEAN MODE)
+        MATCH(name, song_sheet) AGAINST (#{unweighted_query} IN BOOLEAN MODE)
       ORDER BY 
         relevance
       DESC
