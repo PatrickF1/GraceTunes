54180c27ae8f975b70a87889a5806b2077f6a017
leethomas <leet944@gmail.com>
2016-07-22 09:46:37 -0700

    controller test for searching

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class SongsControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+  test "index should retrieve relevant search results" do
+    get :index, sSearch: 'hand', format: :json, xhr: true
+    songs = JSON.parse(@response.body)['data']
+    assert_equal(songs.length, 2)
+  end
 
-  # test "should get edit" do
-  #   get :edit
-  #   assert_response :success
-  # end
-
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 end
