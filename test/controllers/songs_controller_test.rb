7575fafc48beb0af81d48da70e77ea3b82a8f61d
leethomas <leet944@gmail.com>
2016-07-25 21:07:56 -0700

    less flaky test

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class SongsControllerTest < ActionController::TestCase
   test "should get index" do
@@ -7,13 +7,16 @@ class SongsControllerTest < ActionController::TestCase
   end
 
   test "index should retrieve relevant search results" do
-    get :index, sSearch: 'hand', format: :json, xhr: true
-    songs = JSON.parse(@response.body)['data']
-    assert_equal(songs.length, 2)
+    get :index, sSearch: "hand", format: :json, xhr: true
+    songs = JSON.parse(@response.body)["data"]
+    song_names = songs.map { |s| s["name"] }
+    assert_includes(song_names, songs(:hands_to_the_heaven)[:name])
+    assert_includes(song_names, songs(:glorious_day)[:name])
   end
 
   test "should get new" do
     get :new
     assert_response :success
   end
+
 end
