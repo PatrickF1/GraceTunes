effefe2fa03950607c5a9e0200a63647d4a6feb2
Nathan Connor <natyconnor@gmail.com>
2016-11-23 23:18:58 +0000

    Fill first column first. Number of lines in column set to a constant

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,4 +1,7 @@
 module SongsHelper
+  MAX_LINES = 46
+  private_constant :MAX_LINES
+
   def get_class_for_line(line)
     if line == ""
       "blank"
@@ -13,11 +16,10 @@ module SongsHelper
 
   def get_lines_for_columns
     lines = @song.chord_sheet.split("\n")
-    return lines, [] unless lines.length > 20
-    # find a good place to split the two columns if there are more than 20 lines
-    midpoint = lines.length / 2
+    # find a good place to split the two columns
+    midpoint = [MAX_LINES, lines.length-1].min
     until ["lyric", "blank"].include? get_class_for_line(lines[midpoint]) do
-      midpoint += 1
+      midpoint -= 1
     end
     return lines[0 .. midpoint], lines[midpoint + 1 .. -1]
   end
