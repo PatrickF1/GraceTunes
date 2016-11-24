2a32ab978b9e72ed471672d5171c1e1c722d7e8b
Nathan Connor <natyconnor@gmail.com>
2016-11-24 17:53:42 +0000

    Question mark boolean line methods

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -18,7 +18,7 @@ module SongsHelper
     lines = @song.chord_sheet.split("\n")
     # find a good place to split the two columns
     midpoint = [MAX_LINES, lines.length-1].min
-    until "blank" == get_class_for_line(lines[midpoint]) || lyric_line_far_from_blank(midpoint, lines) do
+    until "blank" == get_class_for_line(lines[midpoint]) || lyric_line_far_from_blank?(midpoint, lines) do
       midpoint -= 1
     end
     return lines[0 .. midpoint], lines[midpoint + 1 .. -1]
@@ -26,13 +26,13 @@ module SongsHelper
 
   private
 
-  def lyric_line_far_from_blank(line_num, lines)
+  def lyric_line_far_from_blank?(line_num, lines)
     return false if "lyric" != get_class_for_line(lines[line_num])
-    # if a blank line is less than 5 lines away, this line is too close to a blank
-    true unless line_type_in_range(line_num, "blank", 4, lines) || line_type_in_range(line_num, "blank", -4, lines)
+    # if a blank line is less than 5 lines away in either direction, this line is too close to a blank
+    !(line_type_in_range?(line_num, "blank", 4, lines) || line_type_in_range?(line_num, "blank", -4, lines))
   end
 
-  def line_type_in_range(line_num, line_type, range, lines)
+  def line_type_in_range?(line_num, line_type, range, lines)
     (line_num..line_num + range).each do |num|
       if(line_type == get_class_for_line(lines[num]))
         return true
