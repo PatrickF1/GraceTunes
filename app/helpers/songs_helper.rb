0c9da7438bd3b9b100dc976d6ef1ee8eaacd43eb
Nathan Connor <natyconnor@gmail.com>
2016-11-24 18:56:39 +0000

    fix line_type_in_range? looking outside of range chord_sheet, causing NPE. Also treat end of song as a blank

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,6 @@
 module SongsHelper
   MAX_LINES = 50
+  LINE_PADDING = 4
   private_constant :MAX_LINES
 
   def get_class_for_line(line)
@@ -15,6 +16,7 @@ module SongsHelper
   end
 
   def get_lines_for_columns
+    byebug
     lines = @song.chord_sheet.split("\n")
     # find a good place to split the two columns
     midpoint = [MAX_LINES, lines.length-1].min
@@ -24,21 +26,27 @@ module SongsHelper
     return lines[0 .. midpoint], lines[midpoint + 1 .. -1]
   end
 
-  private
-
   def lyric_line_far_from_blank?(line_num, lines)
     return false if "lyric" != get_class_for_line(lines[line_num])
-    # if a blank line is less than 5 lines away in either direction, this line is too close to a blank
-    !(line_type_in_range?(line_num, "blank", 4, lines) || line_type_in_range?(line_num, "blank", -4, lines))
+    # if both the end, and a blank line is more than 5 lines away in either direction, this line is good to break on
+    line_num < lines.length - LINE_PADDING &&
+      !(line_type_in_range?(line_num, "blank", LINE_PADDING, lines) || line_type_in_range?(line_num, "blank", -LINE_PADDING, lines))
   end
 
   def line_type_in_range?(line_num, line_type, range, lines)
-    (line_num..line_num + range).each do |num|
+    end_range = limit_by_range(0, line_num + range, lines.length - 1)
+    (line_num..end_range).each do |num|
       if(line_type == get_class_for_line(lines[num]))
         return true
       end
     end
     false
+  end
+
+  def limit_by_range(min, value, max)
+    return min if value < min
+    return max if value > max
+    value
   end
 
 end
