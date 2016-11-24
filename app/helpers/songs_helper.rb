6989633fd0528b7922ce718cc702f7b8c4e0053d
Nathan Connor <natyconnor@gmail.com>
2016-11-24 05:05:54 +0000

    Take proximity of nearest blank line when splitting column. Don't want it too close or it's awkward to read

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,5 @@
 module SongsHelper
-  MAX_LINES = 46
+  MAX_LINES = 50
   private_constant :MAX_LINES
 
   def get_class_for_line(line)
@@ -18,9 +18,27 @@ module SongsHelper
     lines = @song.chord_sheet.split("\n")
     # find a good place to split the two columns
     midpoint = [MAX_LINES, lines.length-1].min
-    until ["lyric", "blank"].include? get_class_for_line(lines[midpoint]) do
+    until "blank" == get_class_for_line(lines[midpoint]) || lyric_line_far_from_blank(midpoint, lines) do
       midpoint -= 1
     end
     return lines[0 .. midpoint], lines[midpoint + 1 .. -1]
   end
+
+  private
+
+  def lyric_line_far_from_blank(line_num, lines)
+    return false if "lyric" != get_class_for_line(lines[line_num])
+    # if a blank line is less than 5 lines away, this line is too close to a blank
+    true unless line_type_in_range(line_num, "blank", 4, lines) || line_type_in_range(line_num, "blank", -4, lines)
+  end
+
+  def line_type_in_range(line_num, line_type, range, lines)
+    (line_num..line_num + range).each do |num|
+      if(line_type == get_class_for_line(lines[num]))
+        return true
+      end
+    end
+    false
+  end
+
 end
