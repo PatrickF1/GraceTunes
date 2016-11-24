dc8b334d54e45fc26ae5a292eca4b95d90054f69
Nathan Connor <natyconnor@gmail.com>
2016-11-24 17:59:33 +0000

    only run line_length validation if the song has a chord_sheet

--- app/models/song.rb
+++ app/models/song.rb
@@ -16,7 +16,7 @@ class Song < ActiveRecord::Base
   validates :chord_sheet, presence: true
   validates_inclusion_of :key, in: VALID_KEYS, allow_nil: false
   validates_inclusion_of :tempo, in: VALID_TEMPOS, allow_nil: false
-  validate :line_length
+  validate :line_length, if: -> (song) { song.chord_sheet.present? }
   before_save :normalize, :extract_lyrics
 
   def to_s
@@ -46,7 +46,6 @@ class Song < ActiveRecord::Base
   end
 
   def line_length
-    return if self.chord_sheet.nil?
     self.chord_sheet.split("\n").each do |line|
       if(line.length > MAX_LINE_LENGTH)
         errors.add(:chord_sheet, "has a line that is too long: " + line)
