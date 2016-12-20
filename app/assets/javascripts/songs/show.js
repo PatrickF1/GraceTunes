b83860ac649184eace49d786d752306b21550024
Patrick Fong <patrickf3139@gmail.com>
2016-12-20 09:28:32 -0800

    start work on js

---
+++ app/assets/javascripts/songs/show.js
@@ -0,0 +1,20 @@
+$(function() {
+  $('#transpose_to').on('change', function(e) {
+    var newKey = $('#transpose_to option:selected').val();
+    transposeChordSheet(newKey)
+  });
+
+  var transposeChordSheet = function(newKey) {
+    var songId = window.location.pathname.split('/')[2];
+    var url = `/songs/${songId}.json`;
+
+    $('.show-page .chord-sheet')
+    $.getJSON(url, { newKey }, function(data) {
+      var song = data.song;
+      var chordSheet = song.chord_sheet;
+      var artist = song.artist;
+      var name = song.name;
+    });
+  }
+
+})
