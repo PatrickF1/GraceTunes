172d12bd0c7bbdc9c5a3437f3e391bb4b2d86eed
Patrick Fong <patrickf3139@gmail.com>
2016-12-25 10:11:01 +0900

    make print in current key work

--- app/assets/javascripts/songs/show.js
+++ app/assets/javascripts/songs/show.js
@@ -1,12 +1,14 @@
 $(function() {
   $('#transpose_to').on('change', function(e) {
     var newKey = $('#transpose_to option:selected').val();
-    transposeChordSheet(newKey)
+    transposeChordSheet(newKey);
+
+    var printUrl = getSongBaseUrl() + "/print?new_key=" + newKey;
+    $('#print-btn').attr('href', printUrl);
   });
 
   var transposeChordSheet = function(newKey) {
-    var songId = window.location.pathname.split('/')[2];
-    var url = `/songs/${songId}.json`;
+    var url = getSongBaseUrl() + '.json';
 
     $.getJSON(url, { "new_key": newKey }, function(data) {
       var chordSheet = data.song.chord_sheet;
@@ -14,4 +16,8 @@ $(function() {
     });
   }
 
+  var getSongBaseUrl = function() {
+    var songId = window.location.pathname.split('/')[2];
+    return `/songs/${songId}`;
+  };
 })
