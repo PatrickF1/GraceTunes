f1befd0eaf83e865547de248196a114e4ac4e14f
Winston Kim <winzton.kim@gmail.com>
2017-02-16 15:53:07 -0800

    adding standard scan to show song page and other minor ui changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_sign_in
+  # before_action :require_sign_in
 
   helper_method :current_user
 
