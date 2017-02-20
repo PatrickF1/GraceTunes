bbbe81ca76ec31a011d8f384ba69f24f3df0b602
Patrick Fong <patrickf3139@gmail.com>
2017-02-20 15:56:29 -0800

    use mark.js to highlight matches in the preview pane

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,10 @@
 //
 //= require jquery2
 //= require jquery_ujs
+//= require jquery.mark
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/extras/dataTables.responsive
 //= require turbolinks
 //= require bootstrap-sprockets
+
