939bfcd8041a0a03dc74c61f77c0af563c11c13e
Patrick Fong <patrickf3139@gmail.com>
2017-03-01 17:03:16 -0800

    add comment

--- app/assets/javascripts/songs/index.js
+++ app/assets/javascripts/songs/index.js
@@ -8,6 +8,8 @@ $(function() {
     lengthChange: false,
     ordering: false,
     columns: [
+      // render.text() prevents an XSS vulnerability in dataTables
+      // https://datatables.net/manual/security
       { data: 'name', render: $.fn.dataTable.render.text() },
       { data: 'artist', render: $.fn.dataTable.render.text() },
       { data: 'key' },
