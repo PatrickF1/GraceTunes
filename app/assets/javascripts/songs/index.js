dd348a8925727bc9aee28e6d033b39ca323dd026
Patrick <patrickf3139@gmail.com>
2017-03-02 10:22:00 -0800

    Fix XSS vulnerability (#47)
    escape DataTables name and artist fields

--- app/assets/javascripts/songs/index.js
+++ app/assets/javascripts/songs/index.js
@@ -8,8 +8,10 @@ $(function() {
     lengthChange: false,
     ordering: false,
     columns: [
-      { data: 'name' },
-      { data: 'artist' },
+      // render.text() patches an XSS vulnerability in dataTables
+      // https://datatables.net/manual/security
+      { data: 'name', render: $.fn.dataTable.render.text() },
+      { data: 'artist', render: $.fn.dataTable.render.text() },
       { data: 'key' },
       { data: 'tempo' },
     ],
