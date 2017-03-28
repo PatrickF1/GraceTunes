19b4ec23add71a37a650d4553216cae55cb99a38
Patrick Fong <patrickf3139@gmail.com>
2017-03-28 09:09:29 -0700

    save progress on making roles enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,11 @@
 class User < ActiveRecord::Base
 
-  # No difference between Praise and Admin roles for now
-  # I created an extra one for future-proofing
-  ROLES = %w(Reader Praise Admin)
-
   before_validation :normalize
 
   validates :name, presence: true
   validates :email, presence: true
   validates :role, presence: true
-  validates_inclusion_of :role, in: ROLES, if: -> (user) { user.role.present? }
+  validates_inclusion_of :role, in: Role::VALID_ROLES, if: -> (user) { user.role.present? }
 
   def normalize
     self.name = name.titleize.strip if name
@@ -18,11 +14,11 @@ class User < ActiveRecord::Base
   end
 
   def can_edit?
-    role.in? %w(Praise Admin)
+    role.in? [Role::Praise, Role::Admin]
   end
 
   def can_delete?
-    role == "Admin"
+    role == Role::Admin
   end
 
   def to_s
