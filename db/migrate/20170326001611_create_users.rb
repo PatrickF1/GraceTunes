8760a94b2c28212568a7038165f81df13d49e4a3
Patrick Fong <patrickf3139@gmail.com>
2017-03-28 22:34:00 -0700

    sigh

--- db/migrate/20170326001611_create_users.rb
+++ db/migrate/20170326001611_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users, id: false do |t|
-      # email is supposed to be the primary key but a Rails limitation prevents it from working fully
-      # will be fixed in Rails 5
-      t.string "email", null: false, unique: true
+    # Email is supposed to be the primary key but a Rails bug prevents it from working fully
+    # and it didn't get fixed until Rails 5. However, the combination of a not null and unique
+    # constraint (from the app) essentially implements the primary key constraint.
+    create_table "users", id: false do |t|
+      t.string "email", null: false
       t.timestamps null: false
       t.string "name", null: false
       t.string "role", null: false
