aa60b560a67e4d9c0ff0b583f5caefc9040c2765
Patrick Fong <patrickf3139@gmail.com>
2017-03-30 16:34:50 -0700

    annotate roles and hide buttons users can't use

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,9 @@
 class Role
+  # Readers can perform any read action
   READER = "Reader"
+  # Praise members can create and edit songs
   PRAISE = "Praise"
+  # Admins can delete songs
   ADMIN  = "Admin"
 
   VALID_ROLES = [READER, PRAISE, ADMIN].freeze
