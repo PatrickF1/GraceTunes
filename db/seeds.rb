b400d3e728b01e4dfc965e37487cdf95589271cf
Patrick Fong <patrickf3139@gmail.com>
2017-04-04 17:59:14 -0700

    add more people to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Add GraceTunes devs to users table with admin privileges so they can access the app
+# Add GraceTunes team to users table with admin privileges so they can access all parts of the app
 User.create(
   email: "patrick.fong@gpmail.org",
   name: "Patrick Fong",
@@ -12,5 +12,20 @@ User.create(
 User.create(
   email: "winstons.kim@gpmail.org",
   name: "Winston Kim",
+  role: Role::ADMIN
+)
+User.create(
+  email: "steven.chang2@gpmail.org",
+  name: "Steven Chang",
+  role: Role::ADMIN
+)
+User.create(
+  email: "ivan.yung@gpmail.org",
+  name: "Ivan Yung",
+  role: Role::ADMIN
+)
+User.create(
+  email: "andrew.martinez@gpmail.org",
+  name: "Andrew Martinez",
   role: Role::ADMIN
 )
