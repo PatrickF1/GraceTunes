42edce3641bfaa41d98490fa4148dc8e35d734e3
Nathan Connor <natyconnor@gmail.com>
2017-04-05 19:38:02 +0000

    Add more transposer tests

--- test/services/transposer_test.rb
+++ test/services/transposer_test.rb
@@ -17,6 +17,7 @@ class TransposerTest < ActiveSupport::TestCase
   test "transposing accidental chords works correctly" do
     assert_equal "G", Transposer.send(:transpose_chord, "C", -1, "G#")
     assert_equal "Gb", Transposer.send(:transpose_chord, "C", 3, "Eb")
+    assert_equal "A", Transposer.send(:transpose_chord, "D", -4, "Db")
   end
 
   test "transposing doesn't affect chord modifiers" do
@@ -24,6 +25,8 @@ class TransposerTest < ActiveSupport::TestCase
     assert_equal "Gsus", Transposer.send(:transpose_chord, "A", -2, "Asus")
     assert_equal "Bs", Transposer.send(:transpose_chord, "G", 4, "Gs")
     assert_equal "Dm4", Transposer.send(:transpose_chord, "G", 3, "Bm4")
+    assert_equal "C(b5)", Transposer.send(:transpose_chord, "D", -2, "D(b5)")
+    assert_equal "Bb(b5)", Transposer.send(:transpose_chord, "D", -4, "D(b5)")
   end
 
 end
