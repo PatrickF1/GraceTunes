73536aa0e7d2b2974d37cd79e97473468ef2177e
Nathan Connor <natyconnor@gmail.com>
2017-04-26 00:00:48 +0000

    View with Numbers button now swaps with View with Chords button, while keeping the key select box

--- app/assets/javascripts/songs/show.js
+++ app/assets/javascripts/songs/show.js
@@ -1,16 +1,28 @@
 $(function() {
 
-  $('#transpose_to').on('click', function(e) {
-    var newKey = $('#transpose_to option:selected').val();
-    var tranposeUrl = $("#transpose_to").data('song-url');
-    transposeChordSheet(newKey, tranposeUrl);
-    updatePrintLink(newKey);
+  $('#transpose_to').on('change', function(e) {
+    viewWithChords();
+  });
+
+  $('#to_chords').click(function(e) {
+    viewWithChords();
   });
 
   $('#to_numbers').click(function(e) {
     formatAsNumbers($(this).data("song-url"));
     updatePrintLink("numbers");
+    hideToNumbersButton();
+    showToChordsButton();
   });
+
+  var viewWithChords = function(){
+    var newKey = $('#transpose_to option:selected').val();
+    var tranposeUrl = $("#transpose_to").data('song-url');
+    transposeChordSheet(newKey, tranposeUrl);
+    updatePrintLink(newKey);
+    showToNumbersButton();
+    hideToChordsButton();
+  }
 
   var transposeChordSheet = function(newKey, songUrl) {
     $.getJSON(songUrl, { new_key: newKey }, function(song) {
@@ -38,6 +50,22 @@ $(function() {
     // using $.param to generate query param in order to escape sharps
     var printUrl = $('#print-btn').data('print-url') + "?" + $.param(param)
     $('#print-btn').attr('href', printUrl);
+  }
+
+  var showToNumbersButton = function() {
+    $('.to-numbers-container').show();
+  }
+
+  var showToChordsButton = function() {
+    $('.to-chords-container').show();
+  }
+
+  var hideToNumbersButton = function() {
+    $('.to-numbers-container').hide();
+  }
+
+  var hideToChordsButton = function() {
+    $('.to-chords-container').hide();
   }
 
 });
