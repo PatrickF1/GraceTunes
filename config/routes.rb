46fdbcc535e0ddfb4d20cf966807cb1eace57f81
Patrick Fong <patrickf3139@gmail.com>
2017-05-07 14:24:23 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/songs')
+
+  resources :songs do
+    member do
+      get 'print'
+    end
+  end
+
+  resources :tags
+
+  # Authentication
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: 'sessions#error'
+  get 'signout', to: 'sessions#destroy', as: 'sign_out'
+  get 'signin', to: 'sessions#new', as: 'sign_in'
 end
