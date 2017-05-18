84f1ed746f366a4abcf6a2c43b2edc89db7656ae
Nathan Connor <natyconnor@gmail.com>
2017-05-18 02:52:56 +0000

    Add basic tests and fixtures for praise sets

--- test/models/praise_set_test.rb
+++ test/models/praise_set_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PraiseSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save without name" do
+    set = praise_sets(:hillsong)
+    set.name = nil
+    assert_not set.save, "Saved without a name"
+  end
+
+  test "should not save without owner" do
+    set = praise_sets(:hillsong)
+    set.owner = nil
+    assert_not set.save, "Saved without owner"
+  end
 end
