e481812a63e1e6339ebb3cf8589cf19768a3cfb7
Nathan Connor <natyconnor@gmail.com>
2017-06-30 20:22:53 +0000

    Redo migrations to be after all other migrations and clean up any left overs in schema

--- db/migrate/20170616040459_create_praise_set_songs.rb
+++
@@ -1,9 +0,0 @@
-class CreatePraiseSetSongs < ActiveRecord::Migration[5.1]
-  def change
-    create_table :praise_set_songs do |t|
-      t.references :praise_set
-      t.references :song
-      t.integer :position, null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616040459) do
+ActiveRecord::Schema.define(version: 20170630201424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170616040459) do
     t.string "owner", null: false
     t.string "event_name", null: false
     t.date "event_date", null: false
-    t.integer "song_ids", array: true
     t.text "notes"
     t.index ["event_date"], name: "index_praise_sets_on_event_date"
     t.index ["event_name"], name: "index_praise_sets_on_event_name"
