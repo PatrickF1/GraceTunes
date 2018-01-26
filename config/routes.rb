65228a87b8d103d3ffd82d441e61632c1ddcc170
Patrick <patrickf3139@gmail.com>
2018-01-25 21:36:53 -0800

    show feedback form as an iFrame (#82)

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :tags
+  get "feedback", to: "feedback#show"
 
   # Authentication
   get 'auth/:provider/callback', to: 'sessions#create'
