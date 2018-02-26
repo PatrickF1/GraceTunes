93e475733d692046064d69b073655b16effe0df7
Patrick Fong <patrickf3139@gmail.com>
2018-02-25 23:07:58 -0800

    add migration and write some constraints

---
+++ db/migrate/20180226063342_add_bpm_to_songs.rb
@@ -0,0 +1,18 @@
+class AddBpmToSongs < ActiveRecord::Migration[5.1]
+  def up
+    add_column :songs, :bpm, :integer
+    execute <<-SQL
+              ALTER TABLE
+                songs
+              ADD CONSTRAINT
+                is_valid_bpm
+              CHECK (
+                bpm BETWEEN 1 AND 1000
+              );
+            SQL
+  end
+
+  def down
+    remove_column(:songs, :bpm)
+  end
+end
