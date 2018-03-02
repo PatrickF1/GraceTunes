3091d9296a85ef5ed175c8d5eff57392fe50d40a
Nathan Connor <natyconnor@gmail.com>
2018-03-02 00:00:39 +0000

    Add pagination links. Testing between will_paginate-boostrap and custom made

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,10 +1,25 @@
 class AuditsController < ApplicationController
 
+  PAGE_SIZE = 5
+
   def index
-    page_num = params[:page_num] ? params[:page_num] : 1
-    audits = Audit.history
-    audits = audits.paginate(page: page_num, per_page: 5)
-    @audits_to_song_map = Hash[audits.collect { |audit| [audit, Song.find(audit.auditable_id)] }]
+    @page_num = params[:page_num] ? params[:page_num].to_i : 1
+    @audits = Audit.history
+    @total_pages = ((@audits.length - 1) / PAGE_SIZE) + 1
+    @audits = @audits.paginate(page: @page_num, per_page: PAGE_SIZE)
+
+    @audits_to_song_map = @audits.collect do |audit|
+      song = Song.find_by(id: audit.auditable_id)
+      if song.nil?
+        destroy_audit = Audit.find_by(action: "destroy", auditable_id: audit.auditable_id)
+        song = Song.new(destroy_audit.audited_changes)
+      end
+      [audit, song]
+    end.to_h
+
+    # pagination variables
+    @previous_disabled = @page_num == 1
+    @next_disabled = @page_num == @total_pages
   end
 
   def song_history
--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -7,4 +7,20 @@ module AuditsHelper
       action + "d"
     end
   end
+
+  def text_class_for_audit_action(audit_action)
+    {
+      create: 'text-success',
+      destroy: 'text-danger',
+      update: 'text-warning'
+    }[audit_action.to_sym]
+  end
+
+  def audit_background_for_audit_action(audit_action)
+    {
+      create: 'bg-success',
+      destroy: 'bg-danger',
+      update: 'bg-warning',
+    }[audit_action.to_sym]
+  end
 end
