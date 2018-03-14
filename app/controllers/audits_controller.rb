e3a68ee418a43d8d6da1f40e6bb6e7c2e944fa6a
Nathan Connor <natyconnor@gmail.com>
2018-03-14 16:33:05 +0000

    only need one if statement

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -5,8 +5,8 @@ class AuditsController < ApplicationController
   def index
     @page_num = params[:page_num] ? params[:page_num].to_i : 1
     @audits = Audit.history
-    @audit_action_filter = params[:audit_action] if Audit.valid_action(params[:audit_action])
-    if @audit_action_filter
+    if Audit.valid_action(params[:audit_action])
+      @audit_action_filter = params[:audit_action]
       @audits = @audits.where(action: @audit_action_filter)
     end
     @audits = @audits.paginate(page: @page_num, per_page: PAGE_SIZE)
