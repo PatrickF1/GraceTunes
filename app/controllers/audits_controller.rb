d66786aaf7a5d99abed86db179f8ee847210a305
Patrick Fong <patrickf3139@gmail.com>
2018-04-03 11:38:53 -0700

    use Audit's updates scope

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -27,6 +27,6 @@ class AuditsController < ApplicationController
 
   def song_history
     @song = Song.find(params[:id])
-    @update_audits = @song.audits.where(action: 'update')
+    @update_audits = @song.audits.updates
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -53,7 +53,7 @@ class SongsController < ApplicationController
       Formatter.format_song_nashville(@song)
     end
 
-    @has_been_edited = @song.audits.where(action: 'update').count > 0
+    @has_been_edited = @song.audits.updates.count > 0
 
     respond_to do |format|
       format.html do
