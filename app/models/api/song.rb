f386ba3a0c4e7cbfd74f232d49664d2ba884479f
Patrick Fong <patrickf3139@gmail.com>
2018-05-12 22:59:30 -0700

    use as_json to properly format songs for mobile

--- app/models/api/song.rb
+++ app/models/api/song.rb
@@ -2,19 +2,16 @@ class Api::Song < ApplicationRecord
 
   self.ignored_columns = %w(created_at lyrics spotify_uri)
 
-  def initialize(song)
-    @id = song.id
-    @name = song.name
-    @artist = song.artist
-    @tempo = song.tempo
-    @bpm = song.bpm
-    @standard_scan = song.standard_scan
-    @chord_sheets = {}
-    Song::VALID_KEYS.map do |key|
-      cloned_song = song.clone
+  def as_json(options = nil)
+    chord_sheets = {}
+    Music::MAJOR_KEYS.map do |key|
+      cloned_song = self.clone
       Transposer.transpose_song(cloned_song, key)
-      @chord_sheets[key] = cloned_song.chord_sheet
+      chord_sheets[key] = cloned_song.chord_sheet
     end
-    @chord_sheets['numeral'] = Formatter.format_song_nashville(song)
+    chord_sheets['numeral'] = Formatter.format_song_nashville(self)
+
+    super(except: [:created_at, :key, :chord_sheet, :lyrics, :spotify_uri])
+      .merge(chord_sheets: chord_sheets)
   end
 end
