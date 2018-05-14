93b4c0cb3dff833d6404f7ad06d8aef7f8cbdd37
Patrick Fong <patrickf3139@gmail.com>
2018-05-13 20:24:55 -0700

    small fixes to tests

--- test/controllers/api/songs_controller_test.rb
+++ test/controllers/api/songs_controller_test.rb
@@ -3,7 +3,19 @@ require_relative 'api_controller_test.rb'
 
 class API::SongsControllerTest < API::APIControllerTest
 
-  test 'recently_updated should retrieve only songs updated after date specified' do
+  test 'recently_updated should not be accessible without correct HTTP auth headers' do
+    clear_auth_headers
+    get :recently_updated
+    assert_response :unauthorized
+  end
+
+  test 'recently_updated should retrieve all songs if no date is specified' do
+    get :recently_updated
+    assert_response :success
+    assert(JSON.parse(@response.body).count == 12, "Expecting all 12 song fixtures to be returned")
+  end
+
+  test 'recently_updated should retrieve only the songs updated on or after the date specified' do
     updated_since_value = "2019-01-01 00:00:00"
 
     get :recently_updated, params: {updated_since: updated_since_value}
@@ -11,7 +23,7 @@ class API::SongsControllerTest < API::APIControllerTest
 
     songs_json_object = JSON.parse(@response.body)
     songs_json_object.map do |song_json|
-      assert(song_json['updated_at'] > updated_since_value)
+      assert(song_json['updated_at'] >= updated_since_value)
     end
     assert(
       songs_json_object.count == 4,
@@ -19,16 +31,5 @@ class API::SongsControllerTest < API::APIControllerTest
     )
   end
 
-  test 'recently_updated should retrieve all songs if no date specified' do
-    get :recently_updated
-    assert_response :success
-    assert(JSON.parse(@response.body).count == 12, "Expecting all 12 song fixtures to be returned")
-  end
-
-  test 'recently_updated should not be accessible without correct HTTP auth headers' do
-    clear_auth_headers
-    get :recently_updated
-    assert_response :unauthorized
-  end
 
 end
