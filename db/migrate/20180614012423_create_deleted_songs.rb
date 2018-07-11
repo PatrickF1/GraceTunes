8dd49cffaa753a3e43967ba3f2c1f527903ad0b1
Patrick Fong <patrickf3139@gmail.com>
2018-07-10 18:09:09 -0700

    give deleted_songs a no-defaults primary key

--- db/migrate/20180614012423_create_deleted_songs.rb
+++ db/migrate/20180614012423_create_deleted_songs.rb
@@ -1,9 +1,13 @@
 class CreateDeletedSongs < ActiveRecord::Migration[5.1]
-  def change
-    create_table :deleted_songs, id: false do |t|
-      t.integer "song_id", null: false
+  def up
+    create_table :deleted_songs do |t|
       t.datetime "deleted_at", null: false
       t.string "name", null: false
     end
+    change_column_default :deleted_songs, :id, nil
+  end
+
+  def down
+    drop_table :deleted_songs
   end
 end
