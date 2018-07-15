9a51ae3b3c993f3f5a83d65d2de2b98f4924934e
Patrick Fong <patrickf3139@gmail.com>
2018-07-15 00:15:28 -0700

    fix AuditsHelperTest

--- test/helpers/audits_helper_test.rb
+++ test/helpers/audits_helper_test.rb
@@ -2,11 +2,12 @@ require "test_helper"
 
 class AuditsHelperTest < ActionView::TestCase
   test "filter_empty_audited_changes should select only Array values (diffs) and non-blank values" do
-    deleted_song_create_audit = audits(:deleted_song_audit_1)
-    deleted_song_modify_audit = audits(:deleted_song_audit_2)
-    deleted_song_destroy_audit = audits(:deleted_song_audit_3)
-    assert_equal({"name" => "Delete Me", "key" => "G"}, filter_empty_audited_changes(deleted_song_create_audit.audited_changes))
-    assert_equal({"key" => ["G", "A"], "standard_scan" => ["", "V1."]}, filter_empty_audited_changes(deleted_song_modify_audit.audited_changes))
-    assert_equal({ "name" => "Delete Me", "key" => "A", "standard_scan" => "V1."}, filter_empty_audited_changes(deleted_song_destroy_audit.audited_changes))
+    audited_changes = {
+      name: "Song Name",
+      bpm: nil,
+      artist: "",
+      chord_sheet: "Verse 1\n G"
+    }
+    assert_equal(audited_changes.slice(:name, :chord_sheet), filter_empty_audited_changes(audited_changes))
   end
 end
