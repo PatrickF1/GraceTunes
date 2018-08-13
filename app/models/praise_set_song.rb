ef487c085f06c4fbcf4d47199ebb5ead49157129
Nathan Connor <natyconnor@gmail.com>
2018-08-13 23:35:36 +0000

    use correct field name for praise_set_song song_key

--- app/models/praise_set_song.rb
+++ app/models/praise_set_song.rb
@@ -5,6 +5,6 @@ class PraiseSetSong
 
   validates :song_id, presence: true
   validates :song_key, presence: true
-  validates_inclusion_of :song_key, in: Music::MAJOR_KEYS, if: -> (song) { song.key.present? }
+  validates_inclusion_of :song_key, in: Music::MAJOR_KEYS, if: -> (song) { song.song_key.present? }
 
 end
