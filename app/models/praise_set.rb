46e200138416c9e81c7e446329f6a027941b088c
Patrick Fong <patrickf3139@gmail.com>
2018-08-29 23:08:04 -0700

    switch to using schema directly loaded into code so I can use dynamic enum

--- app/models/praise_set.rb
+++ app/models/praise_set.rb
@@ -1,12 +1,25 @@
 class PraiseSet < ApplicationRecord
+  PRAISE_SET_SONG_SCHEMA = {
+    type: "array",
+    items: {
+      type: "object",
+      required: ["id", "key"],
+      properties: {
+        id: { "type": "number" },
+        key: {
+          type: "string",
+          enum: Song::VALID_KEYS
+        }
+      }
+    }
+  }
+
   belongs_to :owner, :foreign_key => "owner_email", :primary_key => "email", :class_name => "User"
 
   validates :event_name, presence: true
   validates :event_date, presence: true
   validates :owner, presence: true
   validates_inclusion_of :archived, in: [true, false]
-
-  PRAISE_SET_SONG_SCHEMA = Rails.root.join('config', 'schemas', 'praise_set_song.json_schema').to_s
   validates :praise_set_songs, presence: true, json: { schema: PRAISE_SET_SONG_SCHEMA }
 
 end
