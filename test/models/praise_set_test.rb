8e4647eee8a721eca211a9d824a0da756301c9f3
Patrick Fong <patrickf3139@gmail.com>
2018-09-02 23:13:42 -0700

    forgot to change instances of save in PraiseSetTest

--- test/models/praise_set_test.rb
+++ test/models/praise_set_test.rb
@@ -5,31 +5,31 @@ class PraiseSetTest < ActiveSupport::TestCase
   test 'should be invalid without event name' do
     set = praise_sets(:hillsong)
     set.event_name = nil
-    assert_not set.save, 'Validated without a name'
+    assert_not set.valid?, 'Validated without a name'
   end
 
   test 'should be invalid without owner' do
     set = praise_sets(:hillsong)
     set.owner = nil
-    assert_not set.save, 'Was valid without owner'
+    assert_not set.valid?, 'Was valid without owner'
   end
 
   test 'should be invalid if owner does not exist' do
     set = praise_sets(:hillsong)
     set.owner_email = 'doesntexist@gpmail.org'
-    assert_not set.save, 'Was valid with an invalid owner_email'
+    assert_not set.valid?, 'Was valid with an invalid owner_email'
   end
 
   test 'should be invalid without date' do
     set = praise_sets(:hillsong)
     set.event_date = nil
-    assert_not set.save, 'Was valid without date'
+    assert_not set.valid?, 'Was valid without date'
   end
 
   test 'should be invalid without archived status' do
     set = praise_sets(:hillsong)
     set.archived = nil
-    assert_not set.save, 'Was valid without archived'
+    assert_not set.valid?, 'Was valid without archived'
   end
 
   test 'should be valid with the proper praise_set_songs structure' do
@@ -40,38 +40,38 @@ class PraiseSetTest < ActiveSupport::TestCase
   test 'should be invalid if praise_set_songs references songs that do not and have never existed' do
     set = praise_sets(:hillsong)
     set.praise_set_songs[0]["id"] = -1
-    assert_not set.save
+    assert_not set.valid?
   end
 
   test 'should be valid despite referencing deleted songs' do
     set = praise_sets(:hillsong)
     song = Song.find(set.praise_set_songs[0]["id"])
     song.destroy!
-    assert set.save
+    assert set.valid?
   end
 
   test 'should be invalid if praise_set_songs is nil' do
     set = praise_sets(:hillsong)
     set.praise_set_songs = nil
-    assert_not set.save, 'Was valid without praise_set_songs'
+    assert_not set.valid?, 'Was valid without praise_set_songs'
   end
 
   test 'should be invalid if praise_set_songs is missing fields' do
     set = praise_sets(:hillsong)
     set.praise_set_songs[0].delete("id")
-    assert_not set.save, 'Was valid with a praise set song that was missing the id field'
+    assert_not set.valid?, 'Was valid with a praise set song that was missing the id field'
   end
 
   test 'should be invalid if praise_set_songs is an object' do
     set = praise_sets(:hillsong)
     set.praise_set_songs = set.praise_set_songs[0]
-    assert_not set.save
+    assert_not set.valid?
   end
 
   test 'should be invalid if praise_set_songs contains an invalid key' do
     set = praise_sets(:hillsong)
     set.praise_set_songs[0]["key"] = "ABCD"
-    assert_not set.save, 'Was valid with a praise set song that had an invalid key'
+    assert_not set.valid?, 'Was valid with a praise set song that had an invalid key'
   end
 
   test 'retrieve_songs returns referenced Songs' do
