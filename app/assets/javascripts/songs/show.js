f736e4738bc9fe62783548f056e70fd207dcd372
Paulos Liu <paulosliu@users.noreply.github.com>
2023-07-21 14:14:02 -0700

    Allow user to view song without chords (#145)
    Co-authored-by: Paulos Liu <paulos.liu@lytx.com>

--- app/assets/javascripts/songs/show.js
+++ app/assets/javascripts/songs/show.js
@@ -1,65 +1,81 @@
-$(function() {
-
-  $('#transpose_to').on('change', function(e) {
+$(function () {
+  $('#transpose_to').on('change', function (e) {
     viewWithChords();
   });
 
-  $('#to_chords').click(function(e) {
+  $('#to_chords').click(function (e) {
     viewWithChords();
   });
 
-  $('#to_numbers').click(function(e) {
+  $('#chords_toggle').click(function (e) {
+    let enableChords = $(e.target).prop('checked');
+    if(enableChords) {
+      viewWithChords();
+      return;
+    }
+
+    viewNoChords();
+  })
+
+  $('#to_numbers').click(function (e) {
+    viewNumbers();
+  });
+
+  let viewNumbers = function () {
     updateChordSheet({ numbers: true }, $(this).data("song-url"));
     updatePrintLink("numbers");
     hideToNumbersButton();
     showToChordsButton();
-  });
+  }
 
-  var viewWithChords = function() {
-    var newKey = $('#transpose_to option:selected').val();
-    var tranposeUrl = $("#transpose_to").data('song-url');
-    updateChordSheet({ new_key : newKey }, tranposeUrl);
+  let viewNoChords = function () {
+    updatePrintLink("none");
+    updateChordSheet({ disable_chords: true }, $(this).data("song-url"));
+  }
+
+  let viewWithChords = function () {
+    let newKey = $('#transpose_to option:selected').val();
+    let transposeUrl = $("#transpose_to").data('song-url');
+    updateChordSheet({ new_key : newKey }, transposeUrl);
     updatePrintLink(newKey);
     showToNumbersButton();
     hideToChordsButton();
   }
 
-  var updateChordSheet = function(options, songUrl) {
-    $.getJSON(songUrl, options, function(song) {
+  let updateChordSheet = function (options, songUrl) {
+    $.getJSON(songUrl, options, function (song) {
       $('.chord-sheet').html(song.chord_sheet);
     });
   }
 
-  var updatePrintLink = function(newKey) {
-    var param = {}
-    if (newKey == "numbers") {
-      var buttonHtml = $('#print-btn').html();
-      $('#print-btn').html(buttonHtml.replace("in current key", "with numbers"));
+  let updatePrintLink = function (newKey) {
+    let param = {}
+    let $button = $('#print-btn');
+    if (newKey === "none") {
+      param = { disable_chords: true }
+    } else if (newKey === "numbers") {
       param = { numbers: true }
     } else {
-      var buttonHtml = $('#print-btn').html();
-      $('#print-btn').html(buttonHtml.replace("with numbers", "in current key"));
       param = { new_key: newKey }
     }
     // using $.param to generate query param in order to escape sharps
-    var printUrl = $('#print-btn').data('print-url') + "?" + $.param(param)
-    $('#print-btn').attr('href', printUrl);
+    let printUrl = $button.data('print-url') + "?" + $.param(param)
+    $button.attr('href', printUrl);
   }
 
-  var showToNumbersButton = function() {
+  let showToNumbersButton = function () {
     $('.to-numbers-container').show();
   }
 
-  var showToChordsButton = function() {
+  let showToChordsButton = function () {
     $('.to-chords-container').show();
   }
 
-  var hideToNumbersButton = function() {
+  let hideToNumbersButton = function () {
     $('.to-numbers-container').hide();
   }
 
-  var hideToChordsButton = function() {
+  let hideToChordsButton = function () {
     $('.to-chords-container').hide();
   }
-
 });
