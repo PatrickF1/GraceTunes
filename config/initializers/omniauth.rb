dd6eea21cb841812613a4fdf420dc703b8487d33
James Chao <jameschao@gmail.com>
2023-09-08 16:47:56 -0700

    feat: allow acts2.network domain email addresses

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    hd: "gpmail.org" # restrict logins to only Gpmail accounts
+    hd: %w(gpmail.org acts2.network) # restrict logins to only gpmail.org or acts2.network accounts
   }
 end
