bb7669bc509242ab62179f3d28d3207c3cce1eb4
Patrick Fong <patrickf3139@gmail.com>
2024-01-31 20:59:50 -0800

    stop retrieving RAILS_ENV so many times

--- config/puma.rb
+++ config/puma.rb
@@ -11,24 +11,25 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS", 3)
 threads min_threads_count, max_threads_count
 
+env = ENV.fetch("RAILS_ENV", "development")
 # Specifies that the worker count should equal the number of processors in production.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
-if ENV["RAILS_ENV"] == "production"
+if env == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY", Concurrent.physical_processor_count))
   workers worker_count if worker_count > 1
 end
 
+# Specifies the `environment` that Puma will run in.
+environment env
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600 if env == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
-
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV", "development")
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
