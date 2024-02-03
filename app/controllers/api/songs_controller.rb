0df784d6bf8fd012cae1cef9240b07ffb9223cf0
Patrick Fong <patrickf3139@gmail.com>
2024-02-02 22:05:48 -0800

    index works too

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,10 +1,44 @@
 class API::SongsController < API::APIController
+  SONGS_PER_PAGE_DEFAULT = 100
+
   def show
     @song = Song.find(params[:id])
     Song.increment_counter(:view_count, @song.id, touch: false)
     render json: @song
   end
 
+  def index
+    songs = Song
+
+    # perform searching and filtering
+    # pg_search requires search_by_keywords to be to run first before anything else
+    songs = songs.search_by_keywords(params[:keywords]) if params[:keywords].present?
+    songs = songs.where(key: params[:key]) if params[:key].present?
+    songs = songs.where(tempo: params[:tempo]) if params[:tempo].present?
+    songs = songs.select('id, artist, tempo, key, name, chord_sheet, spotify_uri')
+
+    # reorder
+    songs =
+      case params[:sort]
+      when 'Newest First'
+        songs.reorder(created_at: :desc)
+      when 'Most Popular First'
+        songs.reorder(view_count: :desc)
+      else
+        # does nothing if search_by_keywords was run, in which case songs are already ordered by relevance
+        songs.order(name: :asc)
+      end
+
+    # paginate
+    if params[:start].present?
+      page_size = (params[:length] || SONGS_PER_PAGE_DEFAULT).to_i
+      page_num = (params[:start].to_i / page_size.to_i) + 1
+
+      songs = songs.paginate(page: page_num, per_page: page_size)
+    end
+
+    render json: songs
+  end
   def create
     @song = Song.new(song_params)
     if @song.save
