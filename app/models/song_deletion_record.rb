4a81f015f21ab1db7001e58469e6dc1d15f52592
Patrick Fong <patrickf3139@gmail.com>
2024-02-14 22:39:34 -0800

    Run Rubocop autocorrect on app/models

--- app/models/song_deletion_record.rb
+++ app/models/song_deletion_record.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 class SongDeletionRecord < ApplicationRecord
   validates :id, presence: true
   validates :deleted_at, presence: true
   validates :name, presence: true
 
   before_validation do
-    self.deleted_at ||= Time.now
+    self.deleted_at ||= Time.zone.now
   end
-
 end
