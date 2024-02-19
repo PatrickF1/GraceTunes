5f7edbe843aab539df01102db80df23bb948b9a2
Patrick Fong <patrickf3139@gmail.com>
2024-02-19 15:07:56 -0800

    update schema

--- db/migrate/20240219202443_change_email_domain_on_users_and_audits.rb
+++ db/migrate/20240219202443_change_email_domain_on_users_and_audits.rb
@@ -1,6 +1,11 @@
 class ChangeEmailDomainOnUsersAndAudits < ActiveRecord::Migration[7.1]
   def up
-    Users.connection.execute("UPDATE users SET email = replace(email, @gpmail.org, @acts2.network)")
-    Audits.connection.execute("UPDATE audits SET user_id = replace(user_id, @gpmail.org, @acts2.network)")
+    User.connection.execute("UPDATE users SET email = replace(email, '@gpmail.org',  '@acts2.network')")
+    Audited.audit_class.connection.execute("UPDATE audits SET user_id = replace(user_id, '@gpmail.org',  '@acts2.network')")
+  end
+
+  def down
+    User.connection.execute("UPDATE users SET email = replace(email,  '@acts2.network', '@gpmail.org')")
+    Audited.audit_class.connection.execute("UPDATE audits SET user_id = replace(user_id, '@acts2.network', '@gpmail.org')")
   end
 end
