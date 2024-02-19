4a38cf07aa8198b00cf4decb8030ce1027ff15f7
Patrick Fong <patrickf3139@gmail.com>
2024-02-19 15:12:59 -0800

    don't need connection from model

--- db/migrate/20240219202443_change_email_domain_on_users_and_audits.rb
+++ db/migrate/20240219202443_change_email_domain_on_users_and_audits.rb
@@ -1,11 +1,11 @@
 class ChangeEmailDomainOnUsersAndAudits < ActiveRecord::Migration[7.1]
   def up
-    User.connection.execute("UPDATE users SET email = replace(email, '@gpmail.org',  '@acts2.network')")
-    Audited.audit_class.connection.execute("UPDATE audits SET user_id = replace(user_id, '@gpmail.org',  '@acts2.network')")
+    execute("UPDATE users SET email = replace(email, '@gpmail.org',  '@acts2.network')")
+    execute("UPDATE audits SET user_id = replace(user_id, '@gpmail.org',  '@acts2.network')")
   end
 
   def down
-    User.connection.execute("UPDATE users SET email = replace(email,  '@acts2.network', '@gpmail.org')")
-    Audited.audit_class.connection.execute("UPDATE audits SET user_id = replace(user_id, '@acts2.network', '@gpmail.org')")
+    execute("UPDATE users SET email = replace(email,  '@acts2.network', '@gpmail.org')")
+    execute("UPDATE audits SET user_id = replace(user_id, '@acts2.network', '@gpmail.org')")
   end
 end
