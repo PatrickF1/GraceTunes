1988952bd22fdd361d6f08c59fec04be58aa2350
Patrick Fong <patrickf3139@gmail.com>
2024-02-19 23:09:07 -0800

    sign_back_in helper

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -21,10 +21,9 @@ class SessionsControllerTest < ApplicationControllerTest
   end
 
   test "signing in should set authoritative user fields in the session redirect to songs index" do
-    sign_out
     name = "A2N Member"
     email = "member@acts2.network"
-    sign_in(name, email)
+    sign_back_in(name, email)
 
     assert_equal(email, session[:user_email], "Email not set correctly in the session")
     assert_includes(Role::VALID_ROLES, session[:role], "A valid role was not set in the session")
@@ -33,19 +32,17 @@ class SessionsControllerTest < ApplicationControllerTest
   end
 
   test "signing in should pull the user's role from database when it exists" do
-    sign_out
     name = "A2N Member"
     email = "admin@acts2.network"
-    sign_in(name, email)
+    sign_back_in(name, email)
 
     assert_equal(Role::ADMIN, session[:role])
   end
 
   test "signing in correctly sets the user's name in cookie without city extension" do
-    sign_out
     name = "Patrick Fong (Berk/Sf)"
     email = "member@acts2.network"
-    sign_in(name, email)
+    sign_back_in(name, email)
 
     assert_equal('Patrick Fong', cookies[:name])
   end
@@ -56,11 +53,10 @@ class SessionsControllerTest < ApplicationControllerTest
   end
 
   test "create should create never-before-seen users as Readers" do
-    sign_out
     email = "never-before-seen@acts2.network"
 
     assert_difference('User.count', 1, "No new user was created") do
-      sign_in("Never Seen Before", email)
+      sign_back_in("Never Seen Before", email)
     end
     assert_equal(Role::READER, User.find(email).role, "New users should have a role of Reader")
   end
@@ -78,7 +74,8 @@ class SessionsControllerTest < ApplicationControllerTest
 
   private
 
-  def sign_in(name, email)
+  def sign_back_in(name, email)
+    sign_out
     # manually mock the info that would be sent by Google servers
     request.env['omniauth.auth'] = {
       "info" => {
