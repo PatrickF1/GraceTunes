f51a79d00031fb6f9b030c77f306e30fd1322a53
Son Hoang <sonquhoang@gmail.com>
2024-03-28 16:16:58 -0700

    Setting up redux to use for state mangement

--- frontend/graceTunes_Vite/src/main.jsx
+++ frontend/graceTunes_Vite/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
 import App from "./App.jsx"
+import configureStore from "./store"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const store = configureStore()
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
